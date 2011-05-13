f3be62958eb0793abfaa4eb388cff6b73178665d
nicolargo <nicolargo@nicolargo-laptop.(none)>
2011-05-13 09:37:48 +0200

    Add controls / Thruk version 1.0.2

--- shinkenautoinstall-debian.sh
+++ shinkenautoinstall-debian.sh
@@ -7,6 +7,9 @@
 #
 # Syntaxe: root> ./shinkenautoinstall-debian.sh
 #
+# Version 0.4
+#   MaJ de Thruk en 1.0.2
+#
 # Version 0.3
 #   MaJ de Thruk en 1.0.1
 #
@@ -18,90 +21,117 @@
 #   Installation de Shinken 0.5.5
 #   Installation de Thruk 0.90
 #
-script_version="0.3"
+script_version="0.4"
 
-### A modifier
-shinken_version="0.6"	#
-thruk_version="1.0.1"	#
-arch_version="`arch`" 	# Remplacer `arch` par i386 ou x86_64 selon votre architecture
-perl_version="5.10.0" 	# perl_version=`perl -e 'use Config; print $Config{version}'`
-### /A modifier
+### Can be modified
+shinken_version="0.6"				#
+thruk_version="1.0.2"				#
+arch_version="`arch`" 				# i386 | i486 | x86_64
+perl_version="5.10.0" 				# `perl -e 'use Config; print $Config{version}'`
+multiprocessing_version="2.6.2.1"	#
+### /Can be modified
 
 DATE=`date +"%Y%m%d%H%M%S"`
 BACKUP_FILE="/tmp/shinken-backup-$DATE.tgz"
 
-# Fonction: backup
+# Function: backup
 backup() {
   echo "----------------------------------------------------"
-  echo "Backup de la configuration existante"
+  echo "Backup the current configuration"
   echo "----------------------------------------------------"
   echo ""
-  tar zcvf $BACKUP_FILE
+  tar zcvf $BACKUP_FILE /etc/shinken
 }
 
-# Fonction: installation
+# Function: installation
 installation() {
-  echo "----------------------------------------------------"
-  echo "Installation de:"
-  echo " * Shinken version $shinken_version"
-  echo " * Thruk version $thruk_version"
-  echo "----------------------------------------------------"
-  echo ""
-  # Creation du répertoire temporaire
+  # Create the temporary directory
   mkdir ~/$0.$DATE
 
-  # Pre-requis
+  # Pre-requisite
   echo "----------------------------------------------------"
-  echo "Installation de pre-requis"
+  echo "Installation pre-requisites"
+  echo " * python-dev python-setuptools pyro wget libgd2-xpm-dev"
+  echo " * multiprocessing version $multiprocessing_version"
   echo "----------------------------------------------------"
   aptitude install python-dev python-setuptools pyro wget libgd2-xpm-dev
   cd ~/$0.$DATE
-  wget http://pypi.python.org/packages/source/m/multiprocessing/multiprocessing-2.6.2.1.tar.gz
-  tar zxvf multiprocessing-2.6.2.1.tar.gz
-  cd multiprocessing-2.6.2.1/
+  wget http://pypi.python.org/packages/source/m/multiprocessing/multiprocessing-$multiprocessing_version.tar.gz
+  if [ $? ]; then
+  	echo "Download multiprocessing version $multiprocessing_version [OK]"
+  else
+  	echo "Download Shinken version $multiprocessing_version [ERROR]"
+  	exit 1 
+  fi
+  tar zxvf multiprocessing-$multiprocessing_version.tar.gz
+  cd multiprocessing-$multiprocessing_version/
   python setup.py install
 
   # echo "----------------------------------------------------"
-  # echo "Installation et configuration Postfix"
+  # echo "Install and configure Postfix"
   # echo "----------------------------------------------------"
   # aptitude install mailx postfix
   # ln -s /usr/bin/mail /bin/mail
 
-  # Creation de l'utilisateur shinken et du groupe shinken
+  # Download sources
   echo "----------------------------------------------------"
-  echo "Creation utilisateur shinken et groupe shinken"
-  echo "----------------------------------------------------"
-  useradd -s /bin/noshellneeded shinken
-  echo "Fixer un mot de passe pour l'utilisateur shinken"
-  passwd shinken
-  usermod -G shinken shinken
-
-  # Téléchargement
-  echo "----------------------------------------------------"
-  echo "Telechargement"
+  echo "Download sources"
+  echo " * Shinken version $shinken_version"
+  echo " * Thruk version $thruk_version"
   echo "----------------------------------------------------"
   cd ~/$0.$DATE
-  wget http://shinken-monitoring.org/pub/shinken-$shinken_version.tar.gz  
+  wget http://shinken-monitoring.org/pub/shinken-$shinken_version.tar.gz 
+  if [ $? ]; then
+  	echo "Download Shinken version $shinken_version [OK]"
+  else
+  	echo "Download Shinken version $shinken_version [ERROR]"
+  	exit 1 
+  fi
   wget http://www.thruk.org/files/Thruk-$thruk_version-$arch_version-linux-gnu-thread-multi-$perl_version.tar.gz
+  if [ $? ]; then
+  	echo "Download Thruk version $thruk_version [OK]"
+  else
+  	echo "Try another mirror for Thruk (archives)..."
+  	wget http://www.thruk.org/files/archive/Thruk-$thruk_version-$arch_version-linux-gnu-thread-multi-$perl_version.tar.gz
+	if [ $? ]; then
+	  echo "Download Thruk version $shinken_version [OK]"
+	else
+	  echo "Download Thruk version $shinken_version [ERROR]"
+	  exit 1 
+	fi
+  fi
+
+  # Create shinken user and group
+  if [ ! `id -u shinken` ]
+  then
+    echo "----------------------------------------------------"
+    echo "Creation utilisateur shinken et groupe shinken"
+    echo "----------------------------------------------------"
+    useradd -s /bin/noshellneeded shinken
+    echo "Fixer un mot de passe pour l'utilisateur shinken"
+    passwd shinken
+    usermod -G shinken shinken
+  fi
 
   # Installation
   echo "----------------------------------------------------"
-  echo "Installation"
+  echo "Configure, compile and install..."
   echo "----------------------------------------------------"
   cd ~/$0.$DATE
   tar zxvf shinken-$shinken_version.tar.gz
   cd shinken-$shinken_version
   python setup.py install --install-scripts=/usr/bin/  
   cd ~/$0.$DATE
-  tar zxvf Thruk-$thruk_version-`arch`-linux-gnu-thread-multi-$perl_version.tar.gz
+  tar zxvf Thruk-$thruk_version-$arch_version-linux-gnu-thread-multi-$perl_version.tar.gz
   cd Thruk-$thruk_version
   wget http://svn.nicolargo.com/shinkenautoinstall/trunk/thruk_local.conf
   cd ..
+  mkdir /opt/thruk
   cp -R Thruk-$thruk_version /opt/thruk
   chown -R shinken:shinken /opt/thruk
 
   echo "----------------------------------------------------"
-  echo "Hack sur le script init.d de shinken"
+  echo "Hack the default Shinken startup script"
   echo "----------------------------------------------------"
   sed -i 's/BIN="\/usr\/local\/shinken\/bin"/BIN="\/usr\/bin"/g' /etc/init.d/shinken
   sed -i 's/VAR="\/usr\/local\/shinken\/var"/VAR="\/var\/lib\/shinken"/g' /etc/init.d/shinken
@@ -111,7 +141,7 @@ installation() {
   chmod a+rx /etc/init.d/thruk
 
   echo "----------------------------------------------------"
-  echo "Automatisation du lancement au boot"
+  echo "Start Shinken and Thruk on boot"
   echo "----------------------------------------------------"
   update-rc.d shinken defaults
   update-rc.d thruk defaults
@@ -122,7 +152,7 @@ installation() {
 # Fonction: Verifie si les fichiers de conf sont OK
 check() {
   echo "----------------------------------------------------"
-  echo "Vérification de la configuration de Shinken"
+  echo "Check the Shinken configuration"
   echo "----------------------------------------------------"
   python /usr/bin/shinken-arbiter -v -c /etc/shinken/nagios.cfg -c /etc/shinken/shinken-specific.cfg
 }   
@@ -130,39 +160,49 @@ check() {
 # Fonction: Lancement de Shinken
 start() {
   echo "----------------------------------------------------"
-  echo "Lancement de Shinken et de Thruk"
+  echo "Start Shinken and Thruk"
   echo "----------------------------------------------------"
+  sleep 2
   /etc/init.d/shinken start
   /etc/init.d/thruk start
+}
+
+# Fonction: Arret de Shinken
+stop() {
+  echo "----------------------------------------------------"
+  echo "Arret de Shinken et de Thruk"
+  echo "----------------------------------------------------"
+  sleep 2
+  /etc/init.d/shinken stop
+  /etc/init.d/thruk stop
 }
 
 # Fonction: Affiche le résumé de l'installation
 end() {
   echo "----------------------------------------------------"
-  echo "Installation terminée"
+  echo "Installation is finished"
   echo "----------------------------------------------------"
   if [ -f $BACKUP_FILE ]; then
-  echo "Ancienne configuration (backup)   : $BACKUP_FILE"     
+  echo "Backup configuration file         : $BACKUP_FILE"     
   fi
-  echo "Fichiers de configuration         : /etc/shinken"
-  echo "Fichiers de logs                  : /var/lib/shinken/nagios.log"
-  echo "Script de lancement de Shinken    : /etc/init.d/shinken"
-  echo "Script de lancement de Thruk      : /etc/init.d/thruk"
-  echo "Interface d'administration        : http://`hostname`:3000"
+  echo "Configuration file folder         : /etc/shinken"
+  echo "Log file                          : /var/lib/shinken/nagios.log"
+  echo "Shinken startup script            : /etc/init.d/shinken"
+  echo "Thruk startup script              : /etc/init.d/thruk"
+  echo "Thruk web interface URL           : http://`hostname`:3000"
 }
 
 # Programme principal
 if [ "$(id -u)" != "0" ]; then
-	echo "Il faut les droits d'administration pour lancer ce script."
+	echo "This script should be run as root."
 	echo "Syntaxe: sudo $0"
 	exit 1
 fi
-if [ -d /etc/shinken ]; then
+if [ -d /etc/shinken ]; then	
+    stop
     backup
 fi
 installation
 check
 start
 end
-
-
